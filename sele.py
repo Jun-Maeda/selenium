68208df17e5e538700ae8d615b94b2c67937f2aa
MJ <jun.mermaid.voice@gmail.com>
2020-07-14 12:52:46 +0900

    pandas read_csv でのスクレイピング可能"

--- sele.py
+++ sele.py
@@ -2,21 +2,75 @@ import time                            # スリープを使うために必要
 from selenium import webdriver         # Webブラウザを自動操作する（python -m pip install selenium)
 import chromedriver_binary             # パスを通すためのコード
 from selenium.webdriver.chrome.options import Options # オプションを使うためのコード
+import pandas as pd
 
-option = Options()                          # オプションを用意
-option.add_argument('--incognito')  # シークレットモード
 
-driver = webdriver.Chrome(options=option)            # Chromeを準備
 
-#クロームを開く
-def selen(url):
-    driver.get(url)  # Googleを開く
-    time.sleep(5)                          # 5秒間待機
 
-#ショップ名取得関数
-def shop_list():
+
+#マイビジネス地図ページまでスクレイピングしてページ数と何番目かを出力
+#店舗名とキーワードを引数にする
+def selen(shop_name,keyw):
+    count = True
+    page = 1
+    
+    option = Options()                          # オプションを用意
+    option.add_argument('--incognito')  # シークレットモード
+
+    driver = webdriver.Chrome(options=option)            # Chromeを準備
+    
+    driver.get('https://www.google.com/')       # Googleを開く
+    search = driver.find_element_by_name('q')   # HTML内で検索ボックス(name='q')を指定する
+    search.send_keys(keyw)             # 検索ワードを送信する
+    search.submit()                             # 検索を実行
+    
+    time.sleep(5)
+    
+#    マップページに進む
+    url = driver.find_element_by_class_name("DLOTif")
+    url.click()
+    
+    time.sleep(5)
+    
+    
+    try:
+         #店舗名が出てくるまで繰り返す
+        while count:
+    #        表示ページの店舗をすべて取得
+            shop = shop_list(driver)
+            if shop_name in shop:
+                for o in range(len(shop)):
+                    if shop[o] == shop_name:
+                        print(f"{page}ページ")
+                        print(f"{o+1}番目")
+                        count = False
+                break
+            #ない場合
+            else:
+                element = driver.find_element_by_link_text("次へ")
+                link = element.get_attribute("href")
+                #画像のリンクをクリック
+                element.click()
+
+                page += 1       #ページ数をカウント
+
+        #        操作するURLをリンク先に変更
+                driver.get(link)
+                urls = link
+                
+    except:
+        print("なし")
+
+    
+    driver.quit()
+
+    
+    
+
+#ページ中のショップ名取得
+def shop_list(dri):
     #ショップ名を取得
-    shops = driver.find_elements_by_class_name("dbg0pd")
+    shops = dri.find_elements_by_class_name("dbg0pd")
     shop = []
 
     #全てテキストに変換して配列に入れなおす
@@ -26,48 +80,28 @@ def shop_list():
 
 
 
-urls = "https://www.google.com/search?hl=ja&q=%E5%87%BA%E9%9B%B2%E3%80%80%E7%BE%8E%E5%AE%B9%E5%AE%A4&npsic=0&rflfq=1&rlha=0&rllag=35369436,132744442,1407&tbm=lcl&ved=2ahUKEwjv2ZCVwrDqAhUJVN4KHYzgDPMQjGp6BAgLED4&rldoc=1#rlfi=hd:;si:;mv:[[35.3840228,132.76427329999999],[35.3336536,132.7191841]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:2"
-
-
-
-
-
-
-
-page = 1
-name = "・ＭＰＭ・"
-
-count = True
-
-#selen(urls)
-# 
-
-
-#店舗名が出てくるまで繰り返す
-while count:
-    #表示ページに存在するかの確認
-    selen(urls)
-    shop = shop_list()
-    if name in shop:
-        for o in range(len(shop)):
-            if shop[o] == name:
-                print(f"{page}ページ")
-                print(f"{o+1}番目")
-                count = False
-        break
-    #ない場合
-    else:
-        element = driver.find_element_by_link_text("次へ")
-        link = element.get_attribute("href")
-        #画像のリンクをクリック
-        element.click()
+#csvから店舗名とキーワードを取得してスクレイピング
+def shop_keys():
+    df = pd.read_csv('shops.csv', encoding="cp932")
+#    1行ずつデータを取得する
+    rd = df.iterrows()
+#    ある分だけ繰り返す
+    for i,v in rd:
+        name = v["店舗名"]
+        keywd1 = v["キーワード１"]
+        keywd2 = v["キーワード２"]
+        keywd3 = v["キーワード３"]
         
-        page += 1
+        keywds = [keywd1,keywd2,keywd3]
         
-#        操作するURLをリンク先に変更
-        driver.get(link)
-        urls = link
-        
+#        スクレイピングする
+        for n in keywds:
+            selen(name,n)
 
-driver.quit()  
+
+
+if __name__ == "__main__":
+    
+
+
     
---
+++ start.py
@@ -0,0 +1,13 @@
+import time                            # スリープを使うために必要
+from selenium import webdriver         # Webブラウザを自動操作する（python -m pip install selenium)
+import chromedriver_binary             # パスを通すためのコード
+from selenium.webdriver.chrome.options import Options # オプションを使うためのコード
+import pandas as pd
+from sele import selen,shop_keys,shop_list
+
+
+
+
+#selen("・ＭＰＭ・","出雲　美容室")
+shop_keys()
+
